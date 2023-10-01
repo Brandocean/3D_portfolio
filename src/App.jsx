194b85e676d956b3336691a40de53ba777715a25
Brandocean <brandocean2016@gmail.com>
2023-09-30 19:35:17 -0500

    Adding IsAndroid to Tech

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
+import { isAndroid } from 'react-device-detect';
 
-import { About, Contact, Experience, Feedbacks, 
-  Hero, Navbar, Tech, TechPhone, Works, StarsCanvas } from './components';
+import {
+  About, Contact, Experience, Feedbacks,
+  Hero, Navbar, Tech, TechPhone, Works, StarsCanvas
+} from './components';
 
 import Test from "./components/Test";
 
@@ -10,18 +13,22 @@ const App = () => {
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar/>
-          <Hero/>
+          <Navbar />
+          <Hero />
         </div>
-        <About/>
-        <Experience/>
-        {/* <Tech/> */}
-        <TechPhone/>
-        <Works/>
-        <Feedbacks/>
+        <About />
+        <Experience />
+
+        {isAndroid ? (
+          <TechPhone />
+        ) : (
+          <Tech />
+        )}
+        <Works />
+        <Feedbacks />
         <div className="relative z-0">
-          <Contact/>
-          <StarsCanvas/>
+          <Contact />
+          <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
